5478ed4dad969eb90e01373a407f2f246ff04205
muratcanpayir <83983761+muratcanpayir@users.noreply.github.com>
2022-01-25 15:05:45 +0300

    Client (#2)
    * react app created on client folder
    
    * users fetched
    
    * create user form created and added to database
    
    * refactored

---
+++ client/src/App.js
@@ -0,0 +1,75 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/getUsers").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  const createUser = () => {
+    axios
+      .post("http://localhost:3001/createUser", {
+        name,
+        age,
+        username,
+      })
+      .then((response) => {
+        setUsers([...users, { name, age, username }]);
+      });
+    setName("");
+    setAge(0);
+    setUsername("");
+  };
+  return (
+    <div className="App">
+      <div>
+        {users.map((user, index) => {
+          return (
+            <div key={index}>
+              <div>Name: {user.name}</div>
+              <div>Age: {user.age}</div>
+              <div>Username: {user.username}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          value={age}
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
