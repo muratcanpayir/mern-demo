bd083ad227642cca0ce77336491a961d8f459687
Payir <muratcanpayir@gmail.com>
2022-01-25 14:53:21 +0300

    create user form created and added to database

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,70 @@ import axios from "axios";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:3001/getUsers").then((response) => {
       setUsers(response.data);
     });
   }, []);
+
+  const createUser = () => {
+    axios
+      .post("http://localhost:3001/createUser", {
+        name:  name ,
+        age:  age ,
+        username:  username ,
+      })
+      .then((response) => {
+        alert("usercreated");
+      });
+    setName("");
+    setAge(0);
+    setUsername("");
+  };
   return (
     <div className="App">
-      {users.map((user) => {
-        return (
-          <div key={user._id}>
-            <div>Name: {user.name}</div>
-            <div>Age: {user.age}</div>
-            <div>Username: {user.username}</div>
-          </div>
-        );
-      })}
+      <div>
+        {users.map((user) => {
+          return (
+            <div key={user._id}>
+              <div>Name: {user.name}</div>
+              <div>Age: {user.age}</div>
+              <div>Username: {user.username}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          value={age}
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
     </div>
   );
 }
